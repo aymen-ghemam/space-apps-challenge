da771ccc556cf88316c7aef4cd4697cf62207cad
aymen-ghemam <gh.aymen39@gmail.com>
2022-10-02 03:02:51 +0100

    add end screen

---
+++ src/views/Over.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Over = () => {
+  return (
+    <div className='container game-over animate__animated animate__slideInDown'>
+        Game over
+    </div>
+  )
+}
+
+export default Over
