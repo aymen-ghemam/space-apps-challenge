57eedf99f50e4b466382fe561a87f88b866fe41e
yessser <yesser.kellil@gmail.com>
2022-10-02 09:40:08 +0100

    intro and outro player control hide

--- src/views/Intro.js
+++ src/views/Intro.js
@@ -3,7 +3,7 @@ import React, { useEffect, useRef } from 'react'
 const Over = ({setPage}) => {
   return (
     <div className='container'>
-      <video id="intro" controls autoPlay onEnded={()=>setPage(1)} onClick={()=>{}}>
+      <video id="intro"  autoPlay onEnded={()=>setPage(1)} onClick={()=>{}}>
         <source src="/assets/intro.mp4" type="video/mp4" />
         not supported
       </video>
--- src/views/Outro.js
+++ src/views/Outro.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Outro = ({setPage}) => {
   return (
     <div className='container '>
-      <video controls autoPlay={true} onEnded={()=>setPage(0)}>
+      <video  autoPlay={true} onEnded={()=>setPage(0)}>
         <source src="/assets/outro.mp4" type="video/mp4" />
         not supported
       </video>
